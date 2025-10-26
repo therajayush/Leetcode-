30514f85186c9e4ba841a7bb16b1cb03100cbb2a
Raj Ayush <188709341+therajayush@users.noreply.github.com>
2025-10-26 17:17:23 +0530

    Implement overflow check in digit reversal
    Added a check for integer overflow before updating the answer.

--- 7.cpp
+++ 7.cpp
@@ -8,6 +8,10 @@ public:
 
         while(x!=0){
             int digit = x%10;
+
+            // if((ans> INT_MAX/10) || (ans<INT_MIN/10)){
+                return 0;
+            } 
             ans = (ans*10)+digit; 
             x = x/10;
 
